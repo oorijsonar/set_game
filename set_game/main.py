e8a5b2e22fd469ac30429fd55fbecda4c95e6a47
Uri <uri.hershenhorn@imperva.com>
2021-11-08 00:09:24 -0800

    Usability
    Now accepts command line argument for which algorithm we are testing and prints how long on average that algorithm takes to find a set.

--- set_game/main.py
+++ set_game/main.py
@@ -1,10 +1,44 @@
-from algorithm import BruteForce
-from dealer import deal
+import statistics
+from timeit import default_timer as timer
+
+import click
+
+import algorithm
+import dealer
 
 
-def main():
-    board = deal()
-    print(BruteForce(board).find())
+def list_algorithms():
+    return [subclass.__name__ for subclass in algorithm.Algorithm.__subclasses__()]
+
+
+def get_algorithm_class(algorithm_name):
+    return getattr(algorithm, algorithm_name)
+
+
+@click.command()
+@click.option('-a', '--algorithm', 'algorithm_name', type=click.Choice(list_algorithms()), required=True)
+@click.option('-i', '--iterations', type=int, default=1000)
+def main(algorithm_name, iterations):
+    deck = dealer.generate_deck()
+    algorithm_class = get_algorithm_class(algorithm_name)
+
+    times = []
+
+    for _ in range(iterations):
+        board = dealer.deal(deck)
+        start_time = timer()
+        possible_set = algorithm_class(board).find()
+        end_time = timer()
+
+        # TODO: print the board in all the assert failure messages
+        if possible_set is not None:
+            assert possible_set.is_set(), f'Your algorithm said it found a set but {possible_set} is not a set!'
+            # TODO: check if the set is on the board
+        else:
+            assert algorithm.BruteForce(board).find() is None, 'Your algorithm didn\'t find a set that BruteForce did!'
+        times.append(end_time - start_time)
+
+    print(f'Average time: {statistics.mean(times) * 1000} seconds')
 
 
 if __name__ == '__main__':
