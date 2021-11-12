8fd48253cafaab41e5c982216cd3288ec278123c
Uri <uri.hershenhorn@imperva.com>
2021-11-11 22:48:42 -0800

    Convenience functions
    * repr for debugging
    * equality for algorithms to use

--- set_game/card.py
+++ set_game/card.py
@@ -1,3 +1,5 @@
+from textwrap import dedent
+
 from features import Color, Number, Shading, Shape
 
 
@@ -8,3 +10,19 @@ class Card:
         self.number = number
         self.shading = shading
         self.shape = shape
+
+    def __eq__(self, other):
+        return (self.color == other.color and
+                self.number == other.number and
+                self.shading == other.shading and
+                self.shape == other.shape)
+
+    def __repr__(self):
+        return dedent(
+            f'''
+            Color: {self.color.name}
+            Number: {self.number.name}
+            Shading: {self.shading.name}
+            Shape: {self.shape.name}
+            '''
+        )
